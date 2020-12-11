78a2e377f4ad8ffca4e6929802d6d12af23c7872
danielbchen <danielbchen@uchicago.edu>
2020-12-10 23:04:17 -0500

    Loops through webpages to get data for all yrs.

--- Clutch Players.py
+++ Clutch Players.py
@@ -8,22 +8,28 @@ import requests
 
 def clutch_stats_retriever():
     '''
-    Scrapes website to return a dataframe containing data on a player's clutch
-    statistics. 
+    Loops through webpages on inpredictable.com, scrapes data, and returns a
+    dataframe.
     '''
 
-    link = 'http://stats.inpredictable.com/nba/ssnPlayerSplit.php?season=2019&pos=ALL&team=ALL&po=0&frdt=2018-10-16&todt=2019-06-13&shot=both&dst=plyr'
+    years = [str(year) for year in range(1996, 2020)]
+    base_link = 'http://stats.inpredictable.com/nba/ssnPlayerSplit.php?season={}'
 
-    response = requests.get(link)
-    soup = BeautifulSoup(response.text, 'lxml')
+    data_values = []
+    for year in years:
+        response = requests.get(base_link.format(year))
+        soup = BeautifulSoup(response.text, 'lxml')
+        raw_text = soup.find_all('td')
 
-    raw_text = soup.find_all('td')
-    data_values = [item.get_text() for item in raw_text]
-    data_values = data_values[9:]
+        for text in raw_text:
+            contents = text.get_text()
+            data_values.append(contents)
 
-    starting_indexes = [number for number in range(0, 17)]
-    nested_data = [column_getter(index) for index in starting_indexes]
+    indexes_to_drop = [index_retriever(number) for number in range(0, 9)]
+    indexes_to_drop = [item for sublist in indexes_to_drop for item in sublist]
+    data_values = np.delete(data_values, indexes_to_drop)
 
+    nested_data = [column_getter(index) for index in range(0, 17)]
     df = pd.DataFrame(nested_data)
     df = df.transpose()
     df.columns = [
@@ -70,32 +76,7 @@ def column_getter(index):
     
     return values
 
-
-years = [str(year) for year in range(1996, 2020)]
-base_link = 'http://stats.inpredictable.com/nba/ssnPlayerSplit.php?season={}'
-
-data_values = []
-for year in years:
-    response = requests.get(base_link.format(year))
-    soup = BeautifulSoup(response.text, 'lxml')
-    raw_text = soup.find_all('td')
-
-    for text in raw_text:
-        contents = text.get_text()
-        data_values.append(contents)
-
-indexes_to_drop = [index_retriever(number) for number in range(1, 9)]
-indexes_to_drop = [item for sublist in indexes_to_drop for item in sublist]
-data_values = np.delete(data_values, indexes_to_drop)
-
-
-
-
-
-
-
-df = pd.DataFrame(data_values)
-
+clutch_stats_retriever()
 
 
 
